d9a58c9efce5ded90b9f8ceb35c33684de9a3ef5
gbodocares <gbodocares17@gmail.com>
2024-09-06 12:58:58 +0100

    updated the form data

--- studentList.js
+++ studentList.js
@@ -4,29 +4,37 @@ db.collection("students").where('userId', '!=', '')
     .onSnapshot((querySnapshot) => {
         var students = [];
         querySnapshot.forEach((doc) => {
-          students.push(doc.data().formData[0]);
-          console.log("All students: ", students.join(" "))
-          var counter = 1;
-          var allstudents = students.map((student) => {
-            return `
-                <tr>
-                    <td>${counter++}</td>
-                    <td><img src="${student.imageUrl}" style="width: 70px; height: 70px;" /></td>
-                    <td>${student.registrationNo}</td>
-                    <td>${student.surName}</td>
-                    <td>${student.fisrtname}</td>
-                    <td>${student.gender}</td>
-                    <td>${student.lga}</td>
-                    <td>${student.firstcourse}</td>
-                    <td>${student.secondcourse}</td>
-              
-                </tr>
-             
-            `
-          }).join(' ')
-          
-            document.getElementById('geeks').innerHTML = allstudents;
-        });
+                if(doc.data().formData){
+                    students.push(doc.data().formData[0]);
+                    console.log("All students: ", students.join(" "))
+                    var counter = 1;
+                    
+                    var allstudents = students.map((student) => {
+                      return `
+                          <tr>
+                              <td>${counter++}</td>
+                              <td><img src="${student.imageUrl}" style="width: 70px; height: 70px;" /></td>
+                              <td>${student.registrationNo}</td>
+                              <td>${student.surName}</td>
+                              <td>${student.fisrtname}</td>
+                              <td>${student.gender}</td>
+                              <td>${student.lga}</td>
+                              <td>${student.firstcourse}</td>
+                              <td>${student.secondcourse}</td>
+                        
+                          </tr>
+                       
+                      `
+                    }).join(' ')
+                    
+                  
+                } else {
+                    console.log('document does not exist');
+                }
+
+                document.getElementById('geeks').innerHTML = allstudents;
+            });
+        
 });
 
 /*db.collection("students").get().then((querySnapshot) => {
