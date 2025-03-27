058a9bc6519ab323a0b9bf9ccf808446fcb32a30
gbodocares <gbodocares17@gmail.com>
2025-03-27 18:51:03 +0100

    added the Date of birth column

--- studentList.js
+++ studentList.js
@@ -22,6 +22,7 @@ db.collection("students").where('userId', '!=', ' ')
                               <td>${student.firstcourse}</td>
                               <td>${student.secondcourse}</td>
                               <td>${student.phone}</td>
+                              <td>${student.dob}</td>
                               <td>${student.email}</td>
                           </tr>
                        
